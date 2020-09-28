ccc60a65cd9ac8f28d88753714bac5c7850c2e53
Martin Fučík <mar.fucik@gmail.com>
2020-09-28 19:05:46 +0200

    Created a Editor window that opens when double click on Dialogue object and show object name that is selected

---
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace RPG.Dialogue.Editor
+{
+    public class DialogueEditor : EditorWindow
+    {
+        Dialogue selectedDialogue = null;
+
+        [MenuItem("Window/Dialogue Editor")]
+        public static void ShowEditorWindow()
+        {
+            GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
+        }
+
+        [OnOpenAssetAttribute(1)]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+            if (dialogue  != null)
+            {
+                ShowEditorWindow();
+                return true;
+            }
+            return false;
+        }
+
+        private void OnEnable() 
+        {
+            Selection.selectionChanged += OnSelectionChange;
+        }
+
+        private void OnSelectionChange()
+        {
+            Dialogue dialogue = Selection.activeObject as Dialogue;
+            if (dialogue != null)
+            {
+                selectedDialogue = dialogue;
+                Repaint();
+            }
+        }
+
+        private void OnGUI() 
+        {
+            if (selectedDialogue == null)
+            {
+                EditorGUILayout.LabelField("No Dialogue Selected");
+            }
+            else
+            {
+                EditorGUILayout.LabelField(selectedDialogue.name);
+            }
+        }
+    }
+}
